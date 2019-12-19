cc1a4d0a4841c647c305bff0aa544062152f7b18
soffiane <sylar93120@hotmail.fr>
2019-12-19 13:11:16 +0100

    Code review of Pokemon Service

---
+++ src/main/java/fr/fabernovel/pokemango/exception/PokemonNotFoundException.java
@@ -0,0 +1,6 @@
+package fr.fabernovel.pokemango.exception;
+
+public class PokemonNotFoundException extends RuntimeException {
+    public PokemonNotFoundException(String s) {
+    }
+}
--- src/main/java/fr/fabernovel/pokemango/service/PokemonService.java
+++ src/main/java/fr/fabernovel/pokemango/service/PokemonService.java
@@ -1,29 +1,31 @@
 package fr.fabernovel.pokemango.service;
 
+import fr.fabernovel.pokemango.exception.PokemonNotFoundException;
 import fr.fabernovel.pokemango.model.PokemonApiResponse;
 import fr.fabernovel.pokemango.model.PokemonType;
 import fr.fabernovel.pokemango.model.PokemonWithStats;
+import fr.fabernovel.pokemango.model.Stat;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @Service
 public class PokemonService {
 
-    private static final String urlApiPokeman = "https://pokeapi.co/api/v2/pokemon/";
+    private static final String URL_POKEMON_API = "https://pokeapi.co/api/v2/pokemon/";
 
-    @Autowired
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
-    @Autowired
-    public PokemonService() {
+    public PokemonService(@Autowired RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
     /**
@@ -32,85 +34,104 @@ public class PokemonService {
      * @param name The pokemon's name
      * @return The pokemon's details, including stats calculated from other pokemons of the same type(s).
      */
-    public PokemonWithStats getPokemonByName(String name) {
-        PokemonWithStats pokemonS = new PokemonWithStats();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        Map<String, String> params = new HashMap<>();
-        StringBuilder urlS = new StringBuilder();
-        urlS.append(urlApiPokeman);
-
-        if (null != name && "" != name.trim()) {
-            params.put("name", name);
-            urlS.append("/");
-            urlS.append(name);
-        } else {
-            urlS.append("?offset=20");
-            urlS.append("&limit=20");
+    public PokemonWithStats getPokemonByName(String name) throws PokemonNotFoundException {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name must not be null neither empty");
         }
 
-        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("name", name);
 
-        Optional<PokemonApiResponse> pokemon = getBody(params, urlS.toString(), entity, PokemonApiResponse.class);
-
-        if (null == pokemon) {
-            return pokemonS;
-        }
-        PokemonApiResponse pokemonApiResponse = pokemon.get();
-
-        setAverageStats(pokemonApiResponse, restTemplate);
-        setPokemon(pokemonS, pokemonApiResponse);
-
-        System.out.println(pokemonS);
-
-        return pokemonS;
+        return fetchApi(URL_POKEMON_API, queryParams, PokemonApiResponse.class)
+                .map(this::buildPokemonWithStats)
+                .orElseThrow(() -> new PokemonNotFoundException("name: " + name));
     }
 
-    private void setAverageStats(PokemonApiResponse pokemon, RestTemplate restTemplate) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-        List<String> typesUrls = pokemon.getTypes().stream().map(type -> type.getType()).map(t -> t.getUrl()).collect(Collectors.toList());
+    // I'm not sure this function will work
+    public PokemonWithStats getPokemons() {
+        final String requestUrl = URL_POKEMON_API + "?offset=20&limit=20";
 
-        List<PokemonType> pokemonTypes = typesUrls.stream().map(url -> getBody(null, url, entity, PokemonType.class).get()).collect(Collectors.toList());
+        return fetchApi(requestUrl, PokemonApiResponse.class)
+                .map(this::buildPokemonWithStats)
+                .orElseThrow(() -> new PokemonNotFoundException("no pokemons found"));
+    }
 
-        List<PokemonApiResponse> pokemons = pokemonTypes.stream()
-                .flatMap(pokemonType -> pokemonType.getPokemon().stream()
-                        .map(pokemonL -> getBody(null, pokemonL.getPokemon().getUrl(), entity, PokemonApiResponse.class).get())).collect(Collectors.toList());
+    private PokemonWithStats buildPokemonWithStats(PokemonApiResponse pokemonFromApi) {
+        final PokemonWithStats pokemon = this.toPokemonWithStats(pokemonFromApi);
 
-        if (pokemons == null) {
-            return;
-        }
+        List<Integer> stats = fetchAverageStats(pokemonFromApi);
 
-        List<Integer> stats = pokemon.getStats().stream().flatMap(stat -> pokemons.stream()
-                .flatMap(pokemonApiResponse -> pokemonApiResponse.getStats().stream()
-                        .filter(stat1 -> stat1.getStat().getName().equalsIgnoreCase(stat.getStat().getName()))
-                        .map(stat1 -> stat1.getBase_stat())))
+        //final int statAverage = stats.stream().mapToInt((Function.identity()).sum() / stats.size());
+        IntSummaryStatistics statistics = stats.stream().collect(Collectors.summarizingInt(Integer::intValue));
+        final int statAverage = (int) statistics.getAverage();
+        pokemon.getStats().forEach(stat -> stat.setAverageStat(statAverage));
+
+        return pokemon;
+    }
+
+    private List<Integer> fetchAverageStats(PokemonApiResponse pokemon) {
+        List<PokemonApiResponse> pokemons = pokemon.getTypes().stream()
+                .map(typeUrl -> fetchApi(typeUrl.getType().getUrl(), PokemonType.class))
+                .flatMap(this::toStream)
+                .flatMap(pokemonType -> pokemonType.getPokemon().stream())
+                .map(pkmn -> fetchApi(pkmn.getPokemon().getUrl(), PokemonApiResponse.class))
+                .flatMap(this::toStream)
                 .collect(Collectors.toList());
 
-        pokemon.getStats().stream().forEach(pStat -> pStat.setAverageStat(stats.stream().mapToInt(i -> i).sum() / stats.size()));
+        List<Integer> stats = pokemon.getStats().stream().flatMap(stat ->
+                pokemons.stream()
+                        .flatMap(p -> p.getStats().stream())
+                        .filter(statFromApi -> statFromApi.getStat().getName().equalsIgnoreCase(stat.getStat().getName()))
+                        .map(Stat::getBase_stat)
+        ).collect(Collectors.toList());
 
+        return stats;
     }
 
-    private void setPokemon(PokemonWithStats pokemonS, PokemonApiResponse pokemon) {
-        pokemonS.setBase_experience(pokemon.getBase_experience());
-        pokemonS.setHeight(pokemon.getHeight());
-        pokemonS.setId(pokemon.getId());
-        pokemonS.setName(pokemon.getName());
-        pokemonS.setSpecies(pokemon.getSpecies());
-        pokemonS.setSprite_img(pokemon.getSprites().getFront_default());
-        pokemonS.setStats(pokemon.getStats());
+    private <T> Stream<T> toStream(Optional<T> option) {
+        return option.map(Stream::of).orElseGet(Stream::empty);
     }
 
-    private <T> Optional<T> getBody(Map<String, String> params, String urlS, HttpEntity<String> entity, final Class<T> clazz) {
+    private PokemonWithStats toPokemonWithStats(PokemonApiResponse pokemonFromApi) {
+        final PokemonWithStats pokemon = new PokemonWithStats();
+        pokemon.setBase_experience(pokemonFromApi.getBase_experience());
+        pokemon.setHeight(pokemonFromApi.getHeight());
+        pokemon.setId(pokemonFromApi.getId());
+        pokemon.setName(pokemonFromApi.getName());
+        pokemon.setSpecies(pokemonFromApi.getSpecies());
+        pokemon.setSprite_img(pokemonFromApi.getSprites().getFront_default());
+        pokemon.setStats(pokemonFromApi.getStats());
+        return pokemon;
+    }
+
+    private <T> Optional<T> fetchApi(String url, final Class<T> clazz) {
+        return fetchApi(url, null, clazz);
+    }
+
+    private <T> Optional<T> fetchApi(String url, Map<String, String> queryParams, final Class<T> clazz) {
+        // TODO: implement a better management of errors
         try {
-            return Optional.of(restTemplate.exchange(urlS,
+            ResponseEntity<T> response = restTemplate.exchange(
+                    url,
                     HttpMethod.GET,
-                    entity,
-                    clazz, params).getBody());
+                    defaultJsonHeaders(),
+                    clazz, queryParams
+            );
+
+            if (response.getStatusCode().value() == 200) {
+                return Optional.of(response.getBody());
+            } else {
+                return Optional.empty();
+            }
         } catch (IllegalStateException e) {
             return Optional.empty();
         }
+    }
+
+    private HttpEntity<String> defaultJsonHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        return new HttpEntity<String>("parameters", headers);
     }
 
 }
